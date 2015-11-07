8efa63882f24a8a08f428b1816cebead48c0420b
unenglishable <unenglishable@gmail.com>
2015-11-06 20:46:24 -1000

    add helper for parsing gallery requests

---
+++ gallery/helper.js
@@ -0,0 +1,8 @@
+module.exports = {
+  handle: function(error, response, body, callback) {
+    if (error) { callback(error); }
+    else if (response.statusCode === 200) {
+      callback(null, JSON.parse(body).data);
+    }
+  }
+};
