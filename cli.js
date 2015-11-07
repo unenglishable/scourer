e5e98bb5757072a082f003d08559e051a37de3d6
unenglishable <unenglishable@gmail.com>
2015-11-06 20:18:13 -1000

    add sort param to cli

--- cli.js
+++ cli.js
@@ -4,14 +4,18 @@ var gallery = require('./gallery');
 
 program
   .version(require('./package.json').version)
+  .option('-s, --sort <value>', 'time | viral | top', /^time|viral|top/i)
   .option('-a, --all <terms>', 'Search for all of these words')
   .option('-y, --any <terms>', 'Search for any of these words')
   .option('-e, --exactly <phrase>', 'Search for exactly this word or phrase')
   .option('-n, --not <terms>', 'Exclude results matching this')
   .option('-t, --type <type>', 'Show results for any file type, jpg | png | gif | anigif (animated gif) | album', /^(jpg|png|gif|anigif|album)$/i)
-  .option('-s, --size <size>', 'Size ranges, small (500 pixels square or less) | med (500 to 2,000 pixels square) | big (2,000 to 5,000 pixels square) | lrg (5,000 to 10,000 pixels square) | huge (10,000 square pixels and above)', /^(small|med|big|lrg|huge)$/i)
+  .option('-z, --size <size>', 'Size ranges, small (500 pixels square or less) | med (500 to 2,000 pixels square) | big (2,000 to 5,000 pixels square) | lrg (5,000 to 10,000 pixels square) | huge (10,000 square pixels and above)', /^(small|med|big|lrg|huge)$/i)
   .parse(process.argv);
 
+if (typeof program.sort != 'string') {
+  delete program.sort;
+}
 if (typeof program.size != 'string') {
   delete program.size;
 }
@@ -19,6 +23,7 @@ if (typeof program.type != 'string') {
   delete program.type;
 }
 var queryOptions = {
+  sort: program.sort,
   all: program.all,
   any: program.any,
   exactly: program.exactly,
