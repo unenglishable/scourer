8f466242b228d5b8873292b2ec419a04f23cc0c1
unenglishable <unenglishable@gmail.com>
2015-11-06 19:27:02 -1000

    parse params into queryParams

--- query.js
+++ query.js
@@ -4,7 +4,15 @@ var clientId = require('./auth.json')['client_id'];
 var gallerySearchURL = 'https://api.imgur.com/3/gallery/search/';
 
 module.exports = function(params, callback) {
-  var searchURI = URI(gallerySearchURL).query(params).toString();
+  var queryParams = {
+    q_all: params.all,
+    q_any: params.any,
+    q_exactly: params.exactly,
+    q_not: params.not,
+    q_type: params.type,
+    q_size_px: params.size
+  }
+  var searchURI = URI(gallerySearchURL).query(queryParams).toString();
   var options = {
     uri: searchURI,
     headers: {
